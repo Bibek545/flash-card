8d7b0bad913fad2dc8230e99cacc5ed38cd2a957
Bibek Hanal <bbekhamal26@gmail.com>
2025-05-24 14:29:21 +1000

    updated css, html and added basic js function

---
+++ script.js
@@ -0,0 +1,14 @@
+alert ("Welcome to my flash card");
+
+const questions = [
+    {
+        question: 'What is my Name?',
+        answer: 'Bibek Hamal'
+    },
+    {
+        question: 'Where am I from?',
+        answer: 'Nepal'
+    },
+
+]
+
