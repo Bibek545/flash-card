13bb3c518c9fe9c1ddd4ff833ae2baedcc6406da
Bibek Hanal <bbekhamal26@gmail.com>
2025-05-24 16:03:52 +1000

    created objects in arrays, state variables and wrote DOM manipulation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert ("Welcome to my flash card");
+// alert ("Welcome to my flash card");
 
 const questions = [
     {
@@ -12,3 +12,22 @@ const questions = [
 
 ]
 
+// stateing a variables
+let currentQuestionIndex = 0;
+let questionCount = 0;
+
+// DOM elements
+
+const questionElement = document.querySelector(".questions-front p"); //this is for all the question
+const answerElement = document.querySelector(".questions-back p"); // and this is for the answer
+const btns = document.querySelectorAll(".pagination .page-link") // for the next and previous buttons
+const showAnswer = document.querySelector(".buttonShow") // this is for the show answer button
+
+// adding or injecting the question and load them
+
+function loadQuestion() {
+    const current = questions[currentQuestionIndex];
+    questionElement.innerText = `Questions ${currentQuestionIndex + 1}: ${current.question}`;
+
+    option
+}
